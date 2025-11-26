8b307c03ec87f294b47b8448c27a58107292363b
James Costello <jamescostell@gmail.com>
2025-11-26 01:04:42 -0500

    syncing latest changes

--- main.py
+++ main.py
@@ -7,33 +7,23 @@ import wiring
 import pulse
 
 
-# total pixels in entire tree
-PIX_PER_LINE = 50
+# -------------------------
+# HARDWARE CONFIG
+# -------------------------
 
-pixels1 = neopixel.NeoPixel(board.D18, PIX_PER_LINE, auto_write=False, brightness=0.5)
-pixels2 = neopixel.NeoPixel(board.D19, PIX_PER_LINE, auto_write=False, brightness=0.5)
+strips = wiring.strips
+PIXELS_PER_STRAND = wiring.PIXELS_PER_STRAND
+NUM_STRANDS = wiring.NUM_STRANDS
 
-def pixel_index(spine, ring):
-    """
-    returns actual LED index based on spine & ring index.
-    ring 0 = top
-    ring RINGS-1 = bottom
-    """
-    if spine % 2 == 0:
-        # even -> runs top down
-        return spine * PIX_PER_LINE + ring
-    else:
-        # odd -> runs bottom up
-        return spine * PIX_PER_LINE + (PIX_PER_LINE - ring - 1)
+# -------------------------
+# UTILITY FUNCTIONS
+# -------------------------
 
-def set_pixel(spine, ring, color):
-    idx = pixel_index(spine, ring)
-    pixels[idx] = color
+all_pixels = wiring.all_pixels
+show_all = wiring.show_all
+clear = wiring.clear
 
-def clear():
-    for i in range(NUM_PIXELS):
-        pixels[i] = (0,0,0)
-    pixels.show()
+
 
 def fifo(pixels, gap=2, offset=0, color=(0,0,255), sleep=0.1):
     ci = ((255,255,0), (0,0,255), (255,0,0), (0,255,0), (0,255,255), (255,0,255))
@@ -98,27 +88,25 @@ def main():
     args = parse_args()
     try:
         if args.twinkle:
-            pulse.twinkle_stars(150)
+            pulse.twinkle_stars(strips, 150)
         elif args.pulse:
-            pulse.pulse_all()
+            pulse.pulse_all(strips)
         elif args.xmas:
             gaps = (2,3,4,5,10,15,25,35,45)
             while True:
-                for g in gaps:
-                    for i in range(20):
-                        fifo(pixels1, gap=g, offset=i)
-                        fifo(pixels2, gap=g, offset=i)
-                        time.sleep(0.15)
+                for strip in strips:
+                    for g in gaps:
+                        for i in range(20):
+                            fifo(strip, pixels2, gap=g, offset=i)
+                time.sleep(0.15)
         elif args.xmas_twinkle:
-            pulse.xmas_twinkle()
-            
+            # pulse.xmas_twinkle()
+            pulse.twinkle_stars(strips, 150)
+
 
     except KeyboardInterrupt:
         print("\n👋 Exiting")
-        pixels1.fill((0,0,0))
-        pixels2.fill((0,0,0))
-        pixels1.show()
-        pixels2.show()
+        clear(strips)
 
 if __name__ == '__main__':
     main()
