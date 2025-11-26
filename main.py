bc7707eee312a7b4de8dfebe78ff3e252ac36501
James Costello <jamescostell@gmail.com>
2025-11-26 12:45:35 -0500

    syncing latest changes

--- main.py
+++ main.py
@@ -100,7 +100,7 @@ def main():
                         for i in range(20):
                             fifo(strip, pixels2, gap=g, offset=i)
                 time.sleep(0.15)
-        elif args.xmas_twinkle:
+        elif args.twinkle:
             # pulse.xmas_twinkle()
             pulse.twinkle_stars(strips, 150)
 
