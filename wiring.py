94d6481895cb899d733c024089e072c57255b96a
James Costello <jamescostell@gmail.com>
2025-11-27 09:38:58 -0500

    syncing latest changes

--- wiring.py
+++ wiring.py
@@ -29,8 +29,25 @@ strips = [
 def colorize(strips):
     """Loop through and modulo to change the color of each pixel
     take the existing RGB value as reference brightness"""
+    ci = ((255,255,0), (0,0,255), (255,0,0), (0,255,0), (0,255,255), (255,0,255))
     for strip in strips:
-        for i in range(strip.n)
+        for i in range(PIXELS_PER_STRAND):
+            index = i % 0
+            color = ci[index]
+            brightness = strip[i]
+            pix=[]
+            for i in range(3):
+                pix[i] = (brightness[i]/255) * color[i]
+            strip[i] = pix
+                 
+    
+    gap = gap + 1
+    for strip in strips:
+        strip.fill((0,0,0))
+        for i in range(strip.n):
+            index = i % 6
+            color = ci[index]
+
 
 def all_pixels(strips, color):
     """Set every pixel on every strand."""
