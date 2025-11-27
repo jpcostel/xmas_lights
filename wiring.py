454289e8243d482b686e615cf579d7a6d073ac57
James Costello <jamescostell@gmail.com>
2025-11-27 09:28:48 -0500

    syncing latest changes

--- wiring.py
+++ wiring.py
@@ -5,12 +5,14 @@ import neopixel
 # HARDWARE CONFIG
 # -------------------------
 
-PIXELS_PER_STRAND = 100
-NUM_STRANDS = 2
+PIXELS_PER_STRAND = 50
+NUM_STRANDS = 4
 
 DATA_PINS = [
     board.D18,   # Strand 0
     board.D19,   # Strand 1
+    board.D20,   # Strand 2
+    board.D21    # Strand 3
 ]
 
 # Initialize strips
@@ -23,6 +25,12 @@ strips = [
     )
     for i in range(NUM_STRANDS)
 ]
+
+def colorize(strips):
+    """Loop through and modulo to change the color of each pixel
+    take the existing RGB value as reference brightness"""
+    for strip in strips:
+        for i in range(strip.n)
 
 def all_pixels(strips, color):
     """Set every pixel on every strand."""
