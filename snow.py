2505f4177c4710e56e3171344276e0fdac994ea2
James Costello <jamescostell@gmail.com>
2025-12-13 10:02:02 -0500

    syncing latest changes

--- snow.py
+++ snow.py
@@ -7,7 +7,29 @@ SNOW_COLOR = (200, 200, 255)
 MAX_FLAKES = 60
 SPAWN_INTERVAL = 0.4   # seconds
 DT = 0.05              # frame time
-from grid import grid_to_pixel
+
+def grid_to_pixel(row, col):
+    """
+    row: 0 = bottom, 7 = top
+    col: 0 = left, 24 = right
+    returns: (strand, index)
+    """
+
+    if row < 4:
+        strand = 0
+        local_row = row
+    else:
+        strand = 1
+        local_row = row - 4
+
+    if local_row % 2 == 0:
+        # right → left
+        index = local_row * COLS + (COLS - 1 - col)
+    else:
+        # left → right
+        index = local_row * COLS + col
+
+    return strand, index
 
 class Snowflake:
     def __init__(self, col):
