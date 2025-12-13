19b78a90527ea53071b9a43a5482704befddfac5
James Costello <jamescostell@gmail.com>
2025-12-13 10:09:20 -0500

    syncing latest changes

--- snow.py
+++ snow.py
@@ -4,9 +4,9 @@ ROWS = 8
 COLS = 25
 
 SNOW_COLOR = (200, 200, 255)
-MAX_FLAKES = 60
-SPAWN_INTERVAL = 0.4   # seconds
-DT = 0.05              # frame time
+MAX_FLAKES = 100
+SPAWN_INTERVAL = 0.1   # seconds
+DT = 0.033             # frame time
 
 def grid_to_pixel(row, col):
     """
