4366da4830eada424c38a5a4b7ddd8f639778868
James Costello <jamescostell@gmail.com>
2025-12-13 22:02:53 -0500

    syncing latest changes

--- pico_ctrl.py
+++ pico_ctrl.py
@@ -9,6 +9,7 @@ def send_raw(cmd: bytes):
     with serial.Serial(PORT, BAUD, timeout=1) as ser:
         time.sleep(0.2)  # allow port settle
         ser.write(cmd)
+        ser.flush()
 
 def interrupt():
     # Ctrl-C twice to be safe
@@ -20,3 +21,4 @@ def interrupt():
 def run(cmd: str):
     send_raw((cmd + "\n").encode("utf-8"))
     send_raw(("\n").encode("utf-8"))
+    
