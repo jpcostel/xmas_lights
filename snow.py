6ccc37a6fc9ce5992cba9d07194289fa4d14d75a
James Costello <jamescostell@gmail.com>
2025-12-14 20:02:08 -0500

    syncing latest changes

--- snow.py
+++ snow.py
@@ -81,7 +81,7 @@ def snowfall_effect(strips):
             frac = f.y - row
             if 0 <= row < ROWS:
                 accum[row][f.col] += b * frac
-            if 0 <= row - 1 < ROWS:
+            # if 0 <= row - 1 < ROWS:
                 # accum[row - 1][f.col] += b * (1.0 - frac)
 
             alive.append(f)
