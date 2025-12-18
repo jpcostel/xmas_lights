384e380ff751ed8a2ece71258227cc7ad2cb757f
James Costello <jamescostell@gmail.com>
2025-12-17 20:11:13 -0500

    syncing latest changes

--- snow.py
+++ snow.py
@@ -3,7 +3,7 @@ import time, random, math
 ROWS = 8
 COLS = 25
 
-CUTOFF = 0.45        # interpolation cutoff
+CUTOFF = 0.5        # interpolation cutoff
 
 SNOW_COLOR = (255, 255, 200)
 MAX_FLAKES = 150
