21d610fcc8595f7125124d592a685dc3902c1668
James Costello <jamescostell@gmail.com>
2025-12-17 20:17:25 -0500

    syncing latest changes

--- snow.py
+++ snow.py
@@ -5,7 +5,7 @@ COLS = 25
 
 CUTOFF = 0.5        # interpolation cutoff
 
-SNOW_COLOR = (255, 32, 32)
+SNOW_COLOR = (255, 0, 0)
 MAX_FLAKES = 150
 SPAWN_INTERVAL = 0.016   # seconds
 DT = 0.016             # frame time
